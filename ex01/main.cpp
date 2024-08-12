d649838a69f16328608a5f7b0d2a95e02d6b7a1a
Moosa Awad <mawad@lab1r4s8.42abudhabi.ae>
2024-08-13 01:27:14 +0400

    Enhanced the main program as a tester for ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,11 +6,32 @@ int main()
 	Data		sample;
 	uintptr_t	raw;
 
-	sample.grade = 69;
+	std::cout << std::endl;
+	std::cout << "--------------------------------------------------------------------" << std::endl;
+	std::cout << "Serializing the address of the struct in decimal form and storing it" << std::endl;
+	std::cout << "--------------------------------------------------------------------" << std::endl;
+	std::cout << std::endl;
 
 	raw = Serializer::serialize(&sample);
-	std::cout << "Result is -> " << raw << std::endl;
-	
+	std::cout << "Serialized address of the struct is -> " << raw << " (" << &sample << ")\n" << std::endl;
+
+	std::cout << "----------------------------------------------" << std::endl;
+	std::cout << "Directly assigning a grade of 92 to the struct" << std::endl;
+	std::cout << "----------------------------------------------" << std::endl;
+	sample.grade = 92;
+	std::cout << std::endl;
+
+	std::cout << "------------------------------------------------------------------------------" << std::endl;
+	std::cout << "Deserializing the raw stored address of the struct to a pointer of that struct" << std::endl;
+	std::cout << "------------------------------------------------------------------------------" << std::endl;
+	std::cout << std::endl;
+
 	Data*	address = Serializer::deserialize(raw);
-	std::cout << "Result is -> " << address->grade << std::endl;
+	std::cout << "Dereferencing grade on the pointer to the deserialized stored address -> " << address->grade << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "The grade matches and thus, the serialization and deserialization processes on the struct's address were successful" << std::endl;
+	std::cout << std::endl;
+
+	return (0);
 }
